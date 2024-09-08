9220bdfffd3cd274c1794cb86f837f0350a2b952
Miqueias Kawa <kawasousa1554@gmail.com>
2024-09-08 12:41:56 -0300

    Zustand State Management & Map Functionality

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,10 +1,13 @@
 import { ActivityIndicator, FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo';
-import { Link } from 'expo-router';
+import { useUser } from '@clerk/clerk-expo';
 import RideCard from "@/components/RideCard";
 import { icons, images } from "@/constants";
 import GoogleTextInput from "@/components/GoogleTextInput";
+import Map from "@/components/Map";
+import { useLocationsStore } from "@/store";
+import { useEffect, useState } from "react";
+import * as Location from 'expo-location';
 
 const recentRides = [
     {
@@ -107,8 +110,11 @@ const recentRides = [
 
 const Home = () => {
 
+    const { setUserLocation, setDestinationLocation } = useLocationsStore();
     const { user } = useUser();
     const loading = true;
+
+    const [hasPermissions, setHasPermissions] = useState(false);
 
     const handleSignOut = () => {
 
@@ -116,6 +122,29 @@ const Home = () => {
     const handleDestination = () => {
 
     }
+
+    useEffect(() => {
+        const requestLocation = async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status != 'granted') {
+                setHasPermissions(false);
+                return;
+            }
+            let location = await Location.getCurrentPositionAsync();
+            const adress = await Location.reverseGeocodeAsync({
+                latitude: location.coords?.latitude!,
+                longitude: location.coords?.longitude!
+            });
+            setUserLocation({
+                // latitude: location.coords.latitude,
+                // longitude: location.coords.longitude,
+                latitude: 37.78825,
+                longitude: -122.4324,
+                address: `${adress[0].name}, ${adress[0].region}`
+            });
+        }
+        requestLocation();
+    }, []);
 
     return (
         <SafeAreaView className="bg-general-500">
@@ -159,6 +188,15 @@ const Home = () => {
                             containerStyle="bg-white shadow-md shadow-neutral-300"
                             handlePress={handleDestination}
                         />
+
+                        <>
+                            <Text className="text-xl font-JakartaBold mt-5 mb-3">Your Current Location</Text>
+                            <View className="flex flex-row items-center bg-transparent h-[300px]">
+                                <Map />
+                            </View>
+                        </>
+
+                        <Text className="text-xl font-JakartaBold mt-5 mb-3">Recent Rides</Text>
                     </>
                 )}
             />
